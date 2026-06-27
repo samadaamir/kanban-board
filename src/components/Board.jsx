6695273ea3e92946045f3d67a0bb0fe251a59dd8
Samad Aamir <samadaamir3@gmail.com>
2026-06-27 14:00:48 +0500

    board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { initialData } from '../data/initialData'
 const Board = () => {
+
+  const [boardData, setBoardData] = useState(initialData)
   return (
-    <div>Board</div>
+    <div className='w-full flex '>
+    {
+     boardData.columns.map((column)=>(
+      <div key = {column.id} className='w-80 rounded-lg bg-gray-100 p-4 shadow'> 
+      <h2 className='mb-4 text-xl font-bold'>{column.title}</h2>
+      </div>
+     )
+
+     )
+    }
+
+    </div>
   )
 }
 
---
+++ src/data/initialData.js
@@ -0,0 +1,19 @@
+export const initialData = {
+  columns: [
+    {
+      id: "col-1",
+      title: "To Do",
+      cards: [],
+    },
+    {
+      id: "col-2",
+      title: "In Progress",
+      cards: [],
+    },
+    {
+      id: "col-3",
+      title: "Done",
+      cards: [],
+    },
+  ],
+};
